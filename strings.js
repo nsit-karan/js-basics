12123300c12dfa3ada09e14d576456c856859a7e
nsit-karan <nsit_karan@yahoo.com>
2023-06-13 21:59:52 +0530

    iterate ch by ch in a string

--- strings.js
+++ strings.js
@@ -13,5 +13,20 @@ function sortString(str) {
     return str.split("").sort().join("");
 }
 
+/**
+ * for (let i of collection) is the standard way
+ * to iterate over all elements of a collection. 
+ */
+function iterateString(str) {
+    for (let ch of str) {
+        console.log(ch);
+    }
+}
 
 console.log(sortString("hgjkorb"));
+
+// Convert a string to lowercase
+console.log("AbcFh".toLowerCase());
+
+// iterate over all chars in a string
+iterateString("Hi whats up !!");
